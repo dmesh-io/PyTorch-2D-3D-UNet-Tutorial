526e2b020819c4aef41a72710869ac6ae16c2c14
moritz <moritz@dmesh.io>
2022-02-04 16:30:45 +0100

    fix model.py

--- inferencehub/model.py
+++ inferencehub/model.py
@@ -3,7 +3,7 @@ from unet import UNet
 import torch
 
 
-def get_model(weights_path, model_initialization_parameters) -> torch.nn.Module:
+def get_model(weights_path, model_initialization_parameters, map_location="cpu") -> torch.nn.Module:
     model = UNet(**model_initialization_parameters)
     weights = torch.load(weights_path, map_location=map_location)
     model.load_state_dict(weights)  # load weights into model
